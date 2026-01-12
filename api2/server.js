65766917fedf1d60bb9ad92f092b998f2340d750
Aziz ullah <uaziz9164@gmail.com>
2026-01-12 14:54:22 +0500

    convert in to another file

--- api2/server.js
+++ api2/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const connectDB = require("../db");
-const youtubeRouter = require("../route/router");
+const connectDB = require("./db");
+const youtubeRouter = require("./route/router");
 const app = express();
 const cors = require("cors");
+const serverless = require("serverless-http");
+
 
 app.use(
     cors({
@@ -15,22 +17,25 @@ app.use(
 
 app.use(express.json());
 
-let isConnected = false;
+// let isConnected = false;
 
-app.use(async (req, res, next) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-  next();
-});
+// app.use(async (req, res, next) => {
+//   if (!isConnected) {
+//     await connectDB();
+//     isConnected = true;
+//     console.log("Database connected");
+//   }
+//   next();
+// });
 
 app.use('/api/youtube', youtubeRouter);
 
 
-// app.listen(3000, ()=>{
-//      connectDB();
-//     console.log("http://localhost:3000/api/youtube/connectedYoutube")
-// })
+app.listen(3000, ()=>{
+    connectDB();
+    console.log("server is running on port 3000")
+})
 
-module.exports = app;
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
